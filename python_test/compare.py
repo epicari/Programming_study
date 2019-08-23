759f4aaa9d4a0ff3dea28b1d1dd8a4e9b48d881e
suho choi <aiek261247@gmail.com>
2019-08-23 20:58:49 +0900

    test

--- python_test/compare.py
+++ python_test/compare.py
@@ -3,7 +3,7 @@ a = input("비교 할 숫자 입력 A:")
 b = input("비교 할 숫자 입력 B: ")
 c = input("비교 할 숫자 입력 C: ")
 
-result = a < b if b < c else a < c
+result = a < b if b < c else a < c # 2개의 값이 동일 한 경우 예외처리 해야할듯
 if result == True:
     print('B가 두번째로 큼')
 else:
--- python_test/pib_memo.py
+++ python_test/pib_memo.py
@@ -1,6 +1,6 @@
 def pib_m2(n):
     a, b = 1, 0
-    while(range(n)):
+    for i in range(n):
         a, b = b, a + b
     return b
 

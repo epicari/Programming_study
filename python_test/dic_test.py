6b4fc9eca699a61c9ff4198eecbe5f77c04b8061
epicari <aiek261247@gmail.com>
2019-11-26 17:06:33 +0900

    test

--- python_test/dic_test.py
+++ python_test/dic_test.py
@@ -2,6 +2,7 @@
 # 딕셔너리의 key와 value를 입력받고, 찾고자 하는 key를 입력받아 그에 해당하는 value 반환
 # 입력 받은 key에 해당된 value가 없으면 Not found 반환
 # 입력이 없을 경우 break를 걸기 위해 try except 사용
+#
 
 if __name__ == '__main__':
     N = int(input())
--- python_test/extend_split.py
+++ python_test/extend_split.py
@@ -1,6 +1,7 @@
 #
 # s[::2]는 확장된 split로써, 2배수로 자름
 # Hacker를 입력 받으면, Hce akr을 반환
+#
 
 if __name__ == '__main__':
     for _ in range(int(input())):

1a41ac0573469e18264f1b6fbf143c490a9342e1
epicari <aiek261247@gmail.com>
2020-04-10 16:02:02 +0900

    10, Apr, 2020 Study

---
+++ django_test/venv_django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\suhochoi\documents\git\web_study\django_test\venv_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

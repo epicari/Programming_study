23e0a5991ffb980d2bb719aea6949ae84450053f
epicari <aiek261247@gmail.com>
2020-04-10 17:49:08 +0900

    del python venv

--- django_test/venv_django/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\suhochoi\documents\git\web_study\django_test\venv_django\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

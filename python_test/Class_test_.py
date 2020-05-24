76c241bed3e2e98fc9bbcf91ff52e12ca8874a32
epcari <aiek261247@gmail.com>
2020-05-24 17:15:40 +0900

    test

---
+++ python_test/Class_test_.py
@@ -0,0 +1,16 @@
+#
+# Class test
+#
+# https://www.python.org/dev/peps/pep-3107/
+
+"""
+def Func(a: str, b: float = 3.5) -> int:
+    return a + b
+
+print(Func(3)) # 6.5
+print(type(Func(3))) # float
+
+콜론을 사용하여 매개변수에 할당 될 형식 표시(주석)
+화살표를 사용하여 반환값에 할당 될 형식 표시(주석)
+매개변수에 기본값 할당 방법 '변수: 주석 = 기본값' 
+"""

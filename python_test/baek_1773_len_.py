cebf4b0c6dc0bee9868d066882ca19119d230c73
epicari <aiek261247@gmail.com>
2020-06-29 16:49:25 +0900

    29, Jun, 2020 Study, Need to be modified

--- python_test/baek_1773_len_.py
+++ python_test/baek_1773_len_.py
@@ -2,7 +2,7 @@
 # 백준 1773 폭죽쇼 (= 공배수는 한 번만 샘)
 # 두가지 방법이 있으나, 둘 다 시간초과 나옴..
 #
-
+'''
 n, c = map(int, input().split())
 sets = set()
 for _ in range(n):
@@ -10,13 +10,14 @@ for _ in range(n):
     for j in range(m, c+1, m):
         sets.add(j)
 print(len(list(sets)))
-
-check = [False] * (c+1)
-ans = 0
+'''
+n, c = map(int, input().split())
+check = [0] * (c+1)
 for _ in range(n):
     x = int(input())
     for y in range(x, c+1, x):
         if not check[y]:
-            check[y] = True
-            ans += 1
-print(ans)
+            check[y] = 1
+print(sum(check))
+'''
+'''
